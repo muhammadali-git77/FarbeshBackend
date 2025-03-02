3b2afde8c7d92c9adcf4d66602e6b187fd40ecb9
Lochinbek Rasuljonov <lochinbekrasuljonov00@gmail.com>
2025-03-02 22:23:26 +0500

    Update signals.py

--- drivers_admin/signals.py
+++ drivers_admin/signals.py
@@ -7,7 +7,7 @@ import requests
 
 
 
-TELEGRAM_BOT_TOKEN = '7795180366:AAFlB0h52Mf-wkK61ESb6b6__n1c6_pbNgw'  
+TELEGRAM_BOT_TOKEN = '7800984825:AAEPAd2o_vtfrGlUwc7X7g2wHPCbJmriaX8'  
 TELEGRAM_GROUP_ID = '-1002446857055' 
 
 @receiver(post_save, sender=Driver)
