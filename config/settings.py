0ca9c0b7edc8d79ec15a9adcfaf9abcb29424f2b
Muhammadali Abdumannonov <aalixon636@gmail.com>
2025-03-05 01:00:40 +0500

    sessionToken qoshildi

--- config/settings.py
+++ config/settings.py
@@ -172,7 +172,10 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',  
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+          
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  
